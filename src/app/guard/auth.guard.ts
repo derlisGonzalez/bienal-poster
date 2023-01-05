93c38d6726cff4a873597b8e81e994160e7a020e
derlisGonzalez <freddyrube2013@gmail.com>
2023-01-04 21:11:06 -0300

    commit1 04-01-2023

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -42,20 +42,5 @@ export class AuthGuard implements CanActivate, CanLoad {
     return this.verificarAcesso();
   }
 
-  /*canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }*/
-
-  /*canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
-  }*/
-
-
-
-
 
 }
